e8b053a7ba9d2b01168c751b2f4ebacca90e4756
From-pErfo <NM70727@qq.com>
2022-06-05 10:27:02 +0800

    更新正则表达式

--- src/main/java/me/william278/huskhomes2/util/RegexUtil.java
+++ src/main/java/me/william278/huskhomes2/util/RegexUtil.java
@@ -7,6 +7,8 @@ public final class RegexUtil {
     // 添加中文字符支持 [\u4e00-\u9fa5]
     public static final Pattern DESCRIPTION_PATTERN = Pattern.compile("^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$");
     public static final Pattern NAME_PATTERN = Pattern.compile("^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$");
-    public static final Pattern OWNER_NAME_PATTERN = Pattern.compile("^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$\\.^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$");
+	
+	// 玩家名称[基岩版名称前缀.] . 传送名称
+    public static final Pattern OWNER_NAME_PATTERN = Pattern.compile("^[\\._a-zA-Z0-9]+\\.[\\u4e00-\\u9fa5_a-zA-Z0-9]+$");
 
 }
