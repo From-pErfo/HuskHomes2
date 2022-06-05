7626431cf253e136615663fa2388b4e78ec520b7
From-pErfo <NM70727@qq.com>
2022-06-05 09:58:53 +0800

    测试

--- src/main/java/me/william278/huskhomes2/util/RegexUtil.java
+++ src/main/java/me/william278/huskhomes2/util/RegexUtil.java
@@ -4,9 +4,8 @@ import java.util.regex.Pattern;
 
 public final class RegexUtil {
 
-    // 添加中文字符支持 [\u4e00-\u9fa5]
-    public static final Pattern DESCRIPTION_PATTERN = Pattern.compile("^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$");
-    public static final Pattern NAME_PATTERN = Pattern.compile("^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$");
-    public static final Pattern OWNER_NAME_PATTERN = Pattern.compile("^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$\\.^[\\u4e00-\\u9fa5_a-zA-Z0-9]+$");
+    public static final Pattern DESCRIPTION_PATTERN = Pattern.compile("[a-zA-Z0-9\\d\\-_\\s]+");
+    public static final Pattern NAME_PATTERN = Pattern.compile("[A-Za-z0-9_\\-]+");
+    public static final Pattern OWNER_NAME_PATTERN = Pattern.compile("[A-Za-z0-9_]+\\.[A-Za-z0-9_\\-]+");
 
 }
